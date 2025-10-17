7a61acd4f5fb8e4e561df167d51b7bee6e187cf8
Yanis <yanisblotelmazouzi@gmail.com>
2025-10-17 14:25:03 +0200

    Added Calculator Model

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args) {
+        CalculatorModel m = new CalculatorModel();
+        m.addListener((acc, st) -> {
+            System.out.print("ACC=" + acc + " | STACK=");
+            System.out.println(st.isEmpty() ? "[]" : st.toString());
+        });
+
+        m.setAccumulator(12); m.push();   // empile 12
+        m.setAccumulator(7);  m.add();    // 12 + 7 -> ACC=19
+        m.push();                         // empile 19
+        m.setAccumulator(3);  m.mul();    // 19 * 3 -> ACC=57
     }
 }
